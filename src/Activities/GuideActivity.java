566bf583afa3b8269d1c5d2a4bb5a0265950a00a
LNasiba <lutfullaeva.inha@gmail.com>
2016-12-05 14:39:49 +0400

    By Nasiba

--- src/Activities/GuideActivity.java
+++ src/Activities/GuideActivity.java
@@ -2,6 +2,8 @@ package Activities;
 
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,9 +11,13 @@ import java.util.ResourceBundle;
 public class GuideActivity implements Initializable {
 
 
+    public Label Comment;
+    public Button prevImage;
+    public Button nextImage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
 
     }
 }
