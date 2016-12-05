565dc6fa82e704dfc6ebc09bc9ffad1e4473efbd
LNasiba <lutfullaeva.inha@gmail.com>
2016-12-05 18:50:17 +0400

    By Nasiba

--- src/Activities/MainActivity.java
+++ src/Activities/MainActivity.java
@@ -4,6 +4,7 @@ package Activities;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +15,13 @@ public class MainActivity  implements Initializable{
     public TextField LocationTextField;
     public Button searchButton;
     public Button menuButton;
+    public Button hotelButton;
+    public Button restaurantButton;
+    public Button entertainingButton;
+    public Button thingsToDoButton;
+    public Button settingsButton;
+    public Button logInButton;
+    public ImageView image1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
