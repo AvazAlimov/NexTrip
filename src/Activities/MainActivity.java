0928118b348b4e8527b5610cb3b9716055b027d1
LNasiba <lutfullaeva.inha@gmail.com>
2016-12-07 11:08:11 +0400

    By Nasiba

--- src/Activities/MainActivity.java
+++ src/Activities/MainActivity.java
@@ -5,11 +5,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainActivity  implements Initializable{
+
+    public Button button;
+    public HBox ratesBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
